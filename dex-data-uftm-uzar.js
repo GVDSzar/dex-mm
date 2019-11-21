852303e7f3f50302f118a1d50ab7acc35feb01e7
Andre Cronje <andre@fantom.foundation>
2019-11-21 12:03:50 +0200

    Price tracking and weighed towards price

--- dex-data-uftm-uzar.js
+++ dex-data-uftm-uzar.js
@@ -1,6 +1,14 @@
 const { exec } = require('child_process');
+const zar = require('@zar-network/javascript-sdk')
+const ecc = require("tiny-secp256k1")
+const hexEncoding = require("crypto-js/enc-hex")
+const SHA256 = require("crypto-js/sha256")
+const request = require('request')
+const axios = require('axios')
+const PKEY = ''
 
-
+const EC = require('elliptic').ec;
+const CURVE = "secp256k1"
 /*
 
 - id: "1"
@@ -13,35 +21,133 @@ const { exec } = require('child_process');
 const market = "uftm/uzar"
 const targetprice = 1627700
 
-setInterval(fillOrders, 1000)
+const generatePubKey = privateKey => {
+  const curve = new EC(CURVE)
+  const keypair = curve.keyFromPrivate(privateKey)
+  const pubKeyHex = keypair.getPublic(true, 'hex')
+  return {type:"tendermint/PubKeySecp256k1",value:Buffer.from(pubKeyHex, 'hex').toString('base64')}
+}
+
+const sortObject = obj => {
+  if (obj === null) return null
+  if (typeof obj !== "object") return obj
+  // arrays have typeof "object" in js!
+  if (Array.isArray(obj))
+    return obj.map(sortObject)
+  const sortedKeys = Object.keys(obj).sort()
+  const result = {}
+  sortedKeys.forEach(key => {
+    result[key] = sortObject(obj[key])
+  })
+  return result
+}
+const sha256 = (hex) => {
+  if (typeof hex !== "string") throw new Error("sha256 expects a hex string")
+  if (hex.length % 2 !== 0) throw new Error(`invalid hex string length: ${hex}`)
+  const hexEncoded = hexEncoding.parse(hex)
+  return SHA256(hexEncoded).toString()
+}
+const generateSignature = (signBytesHex, privateKey) => {
+  const msgHash = sha256(signBytesHex)
+  const msgHashHex = Buffer.from(msgHash, "hex")
+  const signature = ecc.sign(msgHashHex, Buffer.from(privateKey, "hex")) // enc ignored if buffer
+  return signature.toString("base64")
+}
+
+function serialize(tx) {
+  if (!tx.signatures) {
+    throw new Error("need signature")
+  }
+
+  let msg = tx.msgs[0]
+
+  const stdTx = {
+    tx: {
+      msg: [msg],
+      signatures: tx.signatures,
+      memo: tx.memo,
+      type: tx.type,
+      fee: {
+        amount: [],
+        gas: "200000"
+      }
+    },
+    mode: tx.mode,
+  }
+
+  return JSON.stringify(stdTx)
+}
+
+function sendTransaction(signedTx) {
+  const signedBz = serialize(signedTx)
+  return sendRawTransaction(signedBz)
+}
+
+function sendRawTransaction(signedBz) {
+  const options = {
+    method: "post",
+    url: "http://54.194.212.72:1317/txs",
+    data: signedBz,
+    headers: {
+      "content-type": "text/plain",
+    }
+  }
+  axios.request(options)
+  .then((res) => {
+    console.log(res.data)
+    setTimeout(fillOrders, 1000);
+  })
+  .catch((error) => {
+    console.error(error)
+  })
+}
+
+//setInterval(fillOrders, 1000)
+fillOrders()
 
 function fillOrders() {
 
-  flip = getRandomInt(2)
-  side = "bid"
-  price = getRandomIntInclusive(100000,1627799)
-  quantity = getRandomIntInclusive(1000000,10000000)
+  request('http://54.194.212.72:1317/auth/accounts/xar18x4jd345dwz49rgvjdyupghd4meg9caf0cx7ww', function (error, response, body) {
 
-  console.log(flip)
-  if (flip==0) {
-    side = "ask"
-    price = getRandomIntInclusive(1627701,20000000)
-    quantity = getRandomIntInclusive(1000000,10000000)
-  }
+    if (error) {
+      console.log(error)
+    } else {
+      var json = ''
+      try {
+        json = JSON.parse(body)
+      } catch (err) {
+        console.log(err)
+      }
+      var account_number = json.result.value.account_number
+      var sequence = json.result.value.sequence
+      flip = getRandomInt(2)
+      side = "bid"
+      price = getRandomIntInclusive(100000,1627799)
+      quantity = getRandomIntInclusive(1000000,10000000)
+
+      if (flip==0) {
+        side = "ask"
+        price = getRandomIntInclusive(1627701,20000000)
+        quantity = getRandomIntInclusive(1000000,10000000)
+      }
+
+      var msg = {
+        value: {
+          Direction: side.toUpperCase(),
+          MarketID: "1",
+          Owner: "xar18x4jd345dwz49rgvjdyupghd4meg9caf0cx7ww",
+          Price: price.toString(),
+          Quantity: quantity.toString(),
+          TimeInForce: 600,
+        },
+        type: "order/Post",
+      }
 
 
-  command = "xarcli tx order post 1 "+side+" "+price+" "+quantity+" 600 --from zafx --chain-id=xar-chain-zafx -y"
-  console.log(command)
-  exec(command, (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      return;
+      broadcastTx = sign(PKEY,msg, account_number, sequence)
+      sendTransaction(broadcastTx)
     }
-
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-  });
+  })
 }
 
 
@@ -52,4 +158,76 @@ function getRandomIntInclusive(min, max) {
 }
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+/**
+* generate the sign bytes for a transaction, given a msg
+* @param {Object} concrete msg object
+* @return {Buffer}
+**/
+function getSignBytes(message, msg) {
+ if (!msg) {
+   throw new Error("msg should be an object")
+ }
+ const fee = {
+   amount: [],
+   gas: "200000"
+ }
+
+ const signMsg = {
+   "account_number": message.account_number.toString(),
+   "chain_id": message.chain_id,
+   "fee": fee,
+   "memo": message.memo,
+   "msgs": [msg.value],
+   "sequence": message.sequence.toString(),
+ }
+ console.log(JSON.stringify(sortObject(signMsg)))
+ return Buffer.from(JSON.stringify(sortObject(signMsg)))
+}
+
+/**
+* attaches a signature to the transaction
+* @param {Elliptic.PublicKey} pubKey
+* @param {Buffer} signature
+* @return {Transaction}
+**/
+function addSignature(message, pubKey, signature) {
+ message.signatures = [{
+   pub_key: pubKey,
+   signature: signature,
+ }]
+ return message
+}
+
+/**
+* sign transaction with a given private key and msg
+* @param {string} privateKey private key hex string
+* @param {Object} concrete msg object
+* @return {Transaction}
+**/
+function sign(privateKey, msg, account_number, sequence) {
+ if(!privateKey){
+   throw new Error("private key should not be null")
+ }
+
+ if(!msg){
+   throw new Error("signing message should not be null")
+ }
+
+ var message = {
+   type:'cosmos-sdk/StdTx',
+   sequence:sequence,
+   account_number:account_number,
+   chain_id:'xar-chain-zafx',
+   msgs: [msg],
+   memo: '',
+   source: '',
+   mode: 'sync',
+ }
+
+ const signBytes = getSignBytes(message, msg)
+ const privKeyBuf = Buffer.from(privateKey, "hex")
+ const signature = generateSignature(signBytes.toString("hex"), privKeyBuf)
+ message = addSignature(message, generatePubKey(privKeyBuf), signature)
+ return message
 }
